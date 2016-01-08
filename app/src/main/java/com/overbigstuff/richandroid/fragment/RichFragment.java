28f75453d0bb28a406e46a69d1a7781ad3aacd84
Freelandriy <overbigstuff@gmail.com>
2016-01-08 16:49:16 +0300

    0.0.1 - Добавил документацию для класса RichFragment

--- app/src/main/java/com/overbigstuff/richandroid/fragment/RichFragment.java
+++ app/src/main/java/com/overbigstuff/richandroid/fragment/RichFragment.java
@@ -16,6 +16,18 @@ import com.overbigstuff.richandroid.activity.RichActivity;
 
 import butterknife.ButterKnife;
 
+/**
+ * Fragment class with retain instance and ButterKnife binding.
+ * The class also has convenient methods for adding and replacing the current fragment.
+ * Use with {@link com.overbigstuff.richandroid.activity.RichActivity RichActivity} class.
+ * Set {@link #setRecreateView(boolean)} to true (in {@link #prepare()} method) if you need to recreate the view every time, when the orientation changes.
+ * If {@link #setRecreateView(boolean)} set to true, use {@link #createContent(View)} for creating fragment data (e.g. Adapters, Lists).
+ * Override {@link #initView(View)} for initializing fragment view with fragment data.
+ * If {@link #setRecreateView(boolean)} set to false, {@link #initView(View)} will be invoked once at the first initialization.
+ * Invoke {@link #setToolbarBack()} in {@link #createContent(View)} if you need fully functional back button on the ToolBar.
+ * ToolBar resource id - R.id.fragment_toolbar
+ * Activity ToolBar resource id - R.id.main_toolbar
+ */
 public abstract class RichFragment extends Fragment {
     private View mView;
     private boolean mToolBarBack;
@@ -52,7 +64,7 @@ public abstract class RichFragment extends Fragment {
         }
 
         initToolBar();
-        initAnyway();
+        initAnyway(mView);
     }
 
     private boolean isRecreateView(Bundle savedInstanceState) {
@@ -63,7 +75,7 @@ public abstract class RichFragment extends Fragment {
     public void onDestroy() {
         if (getTargetFragment() != null) {
             if (getTargetFragment() instanceof RichFragment) {
-                ((RichFragment)getTargetFragment()).resume();
+                ((RichFragment) getTargetFragment()).resume();
             }
         }
         super.onDestroy();
@@ -97,14 +109,26 @@ public abstract class RichFragment extends Fragment {
         getRichActivity().switchFragment(clazz, args);
     }
 
+    /**
+     *
+     * @param id - child view id in the root fragment view
+     * @param <T> - type of the child view
+     * @return - view with certain id
+     */
     public <T extends View> T findById(int id) {
         return ButterKnife.findById(mView, id);
     }
 
+    /**
+     * Invoke in {@link #createContent(View)} if you need fully functional back button on the ToolBar
+     */
     protected void setToolbarBack() {
         mToolBarBack = true;
     }
 
+    /**
+     * Invoke in {@link #createContent(View)} if you need menu button on the ToolBar
+     */
     protected void setToolBarMenu() {
         mToolBarBack = false;
     }
@@ -130,7 +154,7 @@ public abstract class RichFragment extends Fragment {
             return;
         }
         activity.setTitle(getActionBarTitle());
-        TextView title = (TextView)mToolbar.findViewById(R.id.toolbar_title);
+        TextView title = (TextView) mToolbar.findViewById(R.id.toolbar_title);
         title.setText(getCenterTitle());
         activity.setSupportActionBar(mToolbar);
         if (mToolBarBack) {
@@ -148,20 +172,47 @@ public abstract class RichFragment extends Fragment {
         return mToolbar;
     }
 
+    /**
+     * Invoked the moment of creating fragment. Override method if you need to change some initial conditions.
+     *
+     * @see #setRecreateView(boolean)
+     */
     public void prepare() {
 
     }
 
-    protected void initAnyway() {
+    /**
+     * Invoked anyway. Use this method if you need to initialize some things every time the orientation changes.
+     * Invoked after {@link #createContent(View) createContent}, {@link #initView(View)  initView}
+     */
+    protected void initAnyway(View view) {
 
     }
 
+    /**
+     * Method for initializing view with fragment data
+     * Invoked every time when fragment re-created, if the {@link #setRecreateView(boolean)} is set to true.
+     * Invoked once time (When the view for the first time created), if the {@link #setRecreateView(boolean)} is set to false.
+     * Invoked after {@link #createContent(View)}
+     * Invoked before {@link #initAnyway(View)}
+     *
+     * @param view - Inflated fragment view.
+     */
     protected abstract void initView(View view);
 
+    /**
+     * Override for fragment data creating(e.g. Adapters, Lists)
+     * Invoked before {@link #initView(View)}, {@link #initAnyway(View)}
+     * Invoked once time (When the view for the first time created)
+     * @param view - Inflated fragment view.
+     */
     protected void createContent(View view) {
 
     }
 
+    /**
+     * Invoked when the upper fragment was destroyed
+     */
     public void resume() {
         if (getActivity().findViewById(R.id.main_toolbar) != null) {
             if (mToolBarBack) {
@@ -171,7 +222,7 @@ public abstract class RichFragment extends Fragment {
             }
             getActivity().setTitle(getActionBarTitle());
             if (getToolbar() != null) {
-                TextView title = (TextView)getToolbar().findViewById(R.id.toolbar_title);
+                TextView title = (TextView) getToolbar().findViewById(R.id.toolbar_title);
                 if (title != null) {
                     title.setText(getCenterTitle());
                 }
@@ -179,14 +230,25 @@ public abstract class RichFragment extends Fragment {
         }
     }
 
+    /**
+     * @return centered title on the ToolBar
+     */
     protected String getCenterTitle() {
-       return "";
+        return "";
     }
 
+    /**
+     * @return standard title on the ToolBar
+     */
     protected String getActionBarTitle() {
-       return "";
+        return "";
     }
 
+    /**
+     * Override for own layout resource
+     *
+     * @return own fragment layout resource id
+     */
     protected abstract int getLayoutId();
 
 }
